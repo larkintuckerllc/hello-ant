6030cd03e9a26a8b4c692ebc193d54e8fae65053
John Tucker <john@larkintuckerllc.com>
2018-02-18 18:06:14 -0500

    Clean

---
+++ clean/src/components/asyncComponent.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default getComponent => (
+  class AsyncComponent extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = { DynamicComponent: null };
+    }
+    componentWillMount() {
+      getComponent().then(({ default: Component }) => {
+        this.setState({ DynamicComponent: Component });
+      });
+    }
+    render() {
+      const { DynamicComponent } = this.state;
+      if (DynamicComponent !== null) {
+        return <DynamicComponent {...this.props} />;
+      }
+      return null;
+    }
+  }
+);
